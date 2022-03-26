676c09b01296c876d50fffcd05a3c21abf042dac
Abdullah Al Masud <masudctg00@gmail.com>
2022-03-26 16:04:19 +0600

    few tweaks

--- src/components/QuestionAndAnswers/QuestionAndAnswers.js
+++ src/components/QuestionAndAnswers/QuestionAndAnswers.js
@@ -6,13 +6,13 @@ const QuestionAndAnswers = () => {
         <div className='px-5 py-5'>
             <Accordion>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>How React Works ?</Accordion.Header>
+                    <Accordion.Header><h5>How React Works ?</h5></Accordion.Header>
                     <Accordion.Body>
                         React creates a virtaul copy of the Document Object Model in memory, which is called Virtaul DOM. They compare the DOM to the virtual DOM if there are any changes. After comparing, they determine where the changes are done and display them in the real DOM accordingly. All of this is possible with help of Diff algorithm. This improves the efficiency of the performance.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>Difference between Props vs State</Accordion.Header>
+                    <Accordion.Header><h5>Difference between Props vs State</h5></Accordion.Header>
                     <Accordion.Body>
                         Props let you to pass data as an argument from one component to another. Props are read-only and immutable
                         so we cannot modify the props from another component.
@@ -21,7 +21,7 @@ const QuestionAndAnswers = () => {
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header>How useState Works ?</Accordion.Header>
+                    <Accordion.Header><h5>How useState Works ?</h5></Accordion.Header>
                     <Accordion.Body>
                         useState is a react hook that returns a stateful value as well as an update function. The setState method is used to update the state when changes have been made. After setState is defined, it receives a new state value and re-renders the components.
                     </Accordion.Body>
